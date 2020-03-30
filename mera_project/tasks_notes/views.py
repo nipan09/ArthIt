aad5e62e8d94f365cd46aedc0d38c560b68fbb6f
nipan09 <pnishant46@gmail.com>
2020-03-30 19:09:08 +0530

    updated balance feature

--- mera_project/tasks_notes/views.py
+++ mera_project/tasks_notes/views.py
@@ -4,12 +4,9 @@ from tasks_notes.forms import RegisterationForm
 from django.contrib.auth import login, logout
 from tasks_notes.models import BudgetInfo
 
+
 def profile_view(request):
     return render(request, 'tasks_notes/index.html')
-'''	user= request.POST['user_name']
-	city= request.POST['user_post']
-	phone= request.POST['user_phone']'''
-	
 
 def login_view(request):
 	if request.method == 'POST':
@@ -17,7 +14,7 @@ def login_view(request):
 		if form.is_valid():
 			user=form.get_user()
 			login(request,user)
-			return render (request, 'tasks_notes/index.html')
+			return redirect('app')
 	else: 
 		form= AuthenticationForm()
 	return render(request, 'registeration/login.html', {'form':form})
@@ -26,9 +23,12 @@ def additem_view(request):
     name = request.POST['expense_name']    
     expense_cost = request.POST['cost']  
     expense_date = request.POST['expense_date']
-    create=BudgetInfo.objects.create(user= request.user,items=name,cost=expense_cost,date_added=expense_date)
-    create.save()
-    post= BudgetInfo.objects.all()
+    try:
+        balance = [int(obj.cost) for obj in BudgetInfo.objects.filter(user=request.user)]
+        BudgetInfo.objects.filter(user=request.user).update(cost=int(balance[0])+int(expense_cost))   
+    except Exception as e:
+        create = BudgetInfo.objects.create(user= request.user,items=name,cost=expense_cost,date_added=expense_date)
+        create.save()
     return redirect('app')
 
 def signup_view(request):
@@ -43,4 +43,6 @@ def signup_view(request):
 	return render(request, 'tasks_notes/signup.html',{'form':form})
  
 def app_view(request):
-	return render(request,'tasks_notes/app.html')
+	budget_qs=BudgetInfo.objects.filter(user=request.user)
+	budget=budget_qs[0].cost
+	return render(request,'tasks_notes/index.html',{'budget':budget})
