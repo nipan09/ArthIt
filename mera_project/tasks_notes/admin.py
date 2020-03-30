4642570740046f309f31a39eebed226ae31c7241
nipan09 <pnishant46@gmail.com>
2020-03-31 00:10:22 +0530

    unwanted lines removed

--- mera_project/tasks_notes/admin.py
+++ mera_project/tasks_notes/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import BudgetInfo
-from .models import UserProfile
 
-admin.site.register(UserProfile)
 admin.site.register(BudgetInfo)
-
-# Register your models here.
--- mera_project/tasks_notes/models.py
+++ mera_project/tasks_notes/models.py
@@ -1,19 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-
-class UserProfile(models.Model):
-	user=models.OneToOneField(User, on_delete= models.CASCADE)
-	city=models.CharField(max_length=100, default='')
-	phone=models.IntegerField(default=0)
 
 class BudgetInfo(models.Model):
-    items= models.CharField(max_length=20)
+    item= models.CharField(max_length=20)
     cost= models.FloatField(blank=False, null=True)
     date_added= models.DateField()
     user= models.ForeignKey(User, on_delete= models.CASCADE)
 
-def create_profile(sender, **kwargs):
-	if kwargs['created']:
-		user_profile=UserProfile.objects.create(user=kwargs['instance'])
-post_save.connect(create_profile, sender=User)
+    def __str__(self):
+    	return str(self.user)
