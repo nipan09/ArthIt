e62b103cbbb04ac1e2cb9a574a69e72099293e2b
nipan09 <pnishant46@gmail.com>
2020-04-09 12:07:17 +0530

    bugs fixed

--- mera_project/tasks_notes/urls.py
+++ mera_project/tasks_notes/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 #app_name= 'tasks_notes'
@@ -9,4 +10,5 @@ urlpatterns= [
       url(r'^signup/$',views.signup_view,name="sign up"),
       url(r'add_item', views.additem_view, name='add item'),
       url(r'^budget-info/(?P<username>\w+)/$', views.app_view, name='app'),
+      url(r'^budget-info/$', views.index_view),
     ]
--- mera_project/tasks_notes/views.py
+++ mera_project/tasks_notes/views.py
@@ -4,8 +4,7 @@ from tasks_notes.forms import RegisterationForm
 from django.contrib.auth import login, logout
 from tasks_notes.models import BudgetInfo
 from datetime import datetime
-from .tasks import monthly_starting_value
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
 def login_view(request):
@@ -28,7 +27,7 @@ def additem_view(request):
     expense_cost = request.POST['cost']
     expense_date = request.POST['expense_date']
     BudgetInfo.objects.create(user= request.user,item=name,cost=expense_cost,date_added=expense_date)
-    return redirect('app')
+    return HttpResponseRedirect(reverse('app', kwargs={'username':request.user}))
 
 def signup_view(request):
 	if request.method == 'POST':
@@ -40,18 +39,17 @@ def signup_view(request):
 	else: 
 		form=RegisterationForm()
 	return render(request, 'tasks_notes/signup.html',{'form':form})
- 
-def app_view(request,username):
+
+def index_view(request):
+	return render(request, 'tasks_notes/index.html')
+
+def app_view(request, username):
 	if request.user.is_authenticated:
 		bal_qs = [int(obj.cost) for obj in BudgetInfo.objects.filter(user=request.user)]
-		now = [i.lstrip("0") for i in datetime.now().strftime("%d")]
+		#now = [i.lstrip("0") for i in datetime.now().strftime("%d")]
 		budget = 0
 		for obj in range(len(bal_qs)):
 			budget=budget+bal_qs[obj]
-		print("budget"+ str(budget))
-		result = monthly_starting_value.delay()
-		abcd = result.get()
-		print(abcd)
 		uzer_qs = BudgetInfo.objects.filter(user=request.user).order_by('-date_added')
 		return render(request,'tasks_notes/index.html',{'budget':budget,'uzer':uzer_qs[0:5]})
 	else:
