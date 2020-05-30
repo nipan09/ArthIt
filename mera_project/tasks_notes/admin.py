1b99d86b21a76939df641718e8a98f989923e79c
nipan09 <pnishant46@gmail.com>
2020-05-30 23:57:43 +0530

    model added

--- mera_project/tasks_notes/admin.py
+++ mera_project/tasks_notes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import BudgetInfo
+from .models import BudgetInfo, UserInfo
 
 admin.site.register(BudgetInfo)
+admin.site.register(UserInfo)
--- mera_project/tasks_notes/models.py
+++ mera_project/tasks_notes/models.py
@@ -9,3 +9,10 @@ class BudgetInfo(models.Model):
 
     def __str__(self):
     	return str(self.user)
+
+class UserInfo(models.Model):
+	name = models.ForeignKey(User, on_delete=models.CASCADE)
+	total_exp = models.FloatField(default = 0)
+
+	def __str__(self):
+		return f'{self.total_exp} of {self.name}'
