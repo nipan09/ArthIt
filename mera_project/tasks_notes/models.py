3410bc58aeed1d00c208dc85e6b232e781fc22f6
nipan09 <pnishant46@gmail.com>
2020-06-03 23:53:41 +0530

    income field added

--- mera_project/tasks_notes/models.py
+++ mera_project/tasks_notes/models.py
@@ -11,8 +11,10 @@ class BudgetInfo(models.Model):
     	return str(self.user)
 
 class UserInfo(models.Model):
-	name = models.ForeignKey(User, on_delete=models.CASCADE)
-	total_exp = models.FloatField(default = 0)
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_exp = models.FloatField(default = 0)
+    income = models.FloatField(default=0)
 
-	def __str__(self):
-		return f'{self.total_exp} of {self.name}'
+    def __str__(self):
+        return str(self.name)
+
