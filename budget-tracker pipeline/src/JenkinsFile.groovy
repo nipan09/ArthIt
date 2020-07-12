edf761937d641fe71f7dc2bda03639d0f6023c64
nipan09 <pnishant46@gmail.com>
2020-07-12 11:22:45 +0530

    initial tests build

--- budget-tracker pipeline/src/JenkinsFile.groovy
+++ budget-tracker pipeline/src/JenkinsFile.groovy
@@ -1,3 +1,36 @@
-
-class JenkinsFile {
+pipeline{
+	agent any
+	stages
+	{
+	  stage('Build'){
+			steps{
+			  echo "Building the project.."
+			  sh'''
+                    source bin/activate
+					pip install -r requirements.txt
+				    deactivate
+                    '''
+			   }
+		}
+		stage('Collect Static files'){
+			steps{
+				echo "Collecting static files.."
+				sh'''
+					source bin/activate
+					python manage.py collectstatic --noinput
+				    deactivate
+				  '''
+			}
+		}
+		stage('Run Unit/ Integration Tests'){
+			steps{
+				echo "Running Unit/Integration Tests.."
+				sh'''
+					source bin/activate
+					python manage.py test
+					deactivate
+				'''
+			}
+		}
+	}
 }
