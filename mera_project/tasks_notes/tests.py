8afa93acbefc2706a7c6d3bbcc7ff8b1dc7fe3a4
nipan09 <pnishant46@gmail.com>
2020-07-12 10:52:02 +0530

    demo test added

--- mera_project/tasks_notes/tests.py
+++ mera_project/tasks_notes/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class FooTest(TestCase):
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+
+	def test_this_will(self):
+		print ('Win')
