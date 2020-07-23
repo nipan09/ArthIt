5c67c8294d8aef448b8f39cbbc04c0a7952cf37a
nipan09 <pnishant46@gmail.com>
2020-07-23 18:12:13 +0530

    email trail #1

--- budget-tracker pipeline/src/JenkinsFile.groovy
+++ budget-tracker pipeline/src/JenkinsFile.groovy
@@ -31,6 +31,7 @@ pipeline{
 		}
 		failure{
 			emailext (
+				to : "nishantpandey9899@gmail.com, pandey.nishant489@gmail.com",
 				subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
 				body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
             <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
